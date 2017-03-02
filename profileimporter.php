96d5837b8b657e0a1439bbd97218b8e5779c4c66
James Kidd <kklolk@gmail.com>
2017-03-02 13:54:23 +1100

    bug fixes.

--- profileimporter.php
+++ profileimporter.php
@@ -4,7 +4,7 @@
  Plugin URI: 
  Description: Allows users to Import BP data from sister wwoof sites. Both must be running same plugin.
  Author: GippslandWeb
- Version: 1.0
+ Version: 1.1
  Author URI: http://gippslandweb.com.au
  GitHub Plugin URI: gippslandweb/gw-bp-profile-importer
  */
@@ -31,11 +31,11 @@
          $user = $_POST['u'];
          $pass = $_POST['p'];
          $url = "http://www.lakes.com.au";
+         //$url = "http://localhost";
          if($_POST['s'] == 'coastal')
             $url = "http://coastalwaters.info";
          global $wpdb;
          $response = wp_remote_post($url."/wp-json/gwnb/v1/export", array('method' => 'POST', 'body' => array('u' => $user, 'p' => $pass, 's' => $this->sharedKey)));
-
          if(is_wp_error($response)) {
              echo "Something went wrong";
          }
@@ -46,23 +46,27 @@
                  echo 'failure';
                  return;
              }
+             
              foreach($data->data as $d) {
                  //check its type against type
-                 if($wpdb->get_result('SELECT type from wp_bp_xprofile_fields WHERE id = '.$d->field_id)->type != $d->type){
-                     echo "type mismatch skipping field: ".$d->field_id;
+		$t = $wpdb->get_row('SELECT type from wp_bp_xprofile_fields WHERE id = '.$d->field_id);
+                 if($t &&  $t->type != $d->type){
+                     echo "|| type mismatch skipping field: ".$d->field_id;
                      continue;
                  }
                  $r = $wpdb->update(
                      'wp_bp_xprofile_data',
                      array('value' => $d->value, 'last_updated' => current_time('mysql')),
                      array('field_id'=> $d->field_id, 'user_id' => get_current_user_id()),
-                     array('%d','%s'),
+                     array('%s','%d'),
                      array('%d','%d'));
-
+echo "||updated field".$d->field_id." ".$d->value;
                if($r === false) {
                    //error updating insert it
-                $wpdb->insert('wp_bp_xprofile_data', array('field_id'=> $d->field_id, 'user_id' => get_current_user_id(), 'value' => $d->value, 'last_updated' => current_time('mysql')),array());
-               }
+                $wpdb->insert('wp_bp_xprofile_data', array('field_id'=> $d->field_id, 'user_id' => get_current_user_id(), 'value' => $d->value, 'last_updated' => current_time('mysql')));
+echo "||Inserted field".$d->field_id." ".$d->value;              
+ }
+
              }
          }
      }
@@ -70,7 +74,6 @@
 
      function ExportUser() {
          global $wpdb;
-
          $results = array();
          $results['result'] = false;
          $u = $_POST['u'];
@@ -82,12 +85,12 @@
          $user = wp_authenticate($u,$p);
          if($user) {
              $results['result'] = true;
-             $sql = "SELECT `field_id`, `user_id`, `value`, `last_updated`,`wp_bp_xprofile_fields`.`type` FROM wp_bp_xprofile_data INNER JOIN wp_bp_xprofile_fields 
+             $sql = "SELECT `field_id`, `user_id`, `value`, `last_updated`, `type` FROM wp_bp_xprofile_data INNER JOIN wp_bp_xprofile_fields 
 ON (wp_bp_xprofile_data.field_id = wp_bp_xprofile_fields.id) WHERE user_id = '$user->ID'";
 
 //             $sql = "SELECT * FROM wp_bp_xprofile_data WHERE user_id = '$user->ID'";
              $res = $wpdb->get_results($sql);
-
+             
              $results['data'] = $res;
          }
          else {
